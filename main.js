37d546a3e69e791f1e0e9ed93c9e30de1e215460
k3naks <shibeko.egor@gmail.com>
2016-08-22 16:27:21 +0300

    Updated

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
 mongoose.Promise = global.Promise;
 
@@ -18,7 +18,7 @@ var TodoSchema = new mongoose.Schema({
    name: String,
    completed: Boolean,
    note: String,
-   updated_at: { type: Date, default: Date.now },
+   updated_at: { type: Date, default: Date.now }
 });
 // Create a model based on the schema
 var Todo = mongoose.model('Todo', TodoSchema);
